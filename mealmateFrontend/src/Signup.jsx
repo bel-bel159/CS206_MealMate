c0c02b45b17590347aaf3f158b6713840ef3a570
Eaint <haeaint.2022/scis.smu.edu.sg>
2024-03-05 15:32:10 +0800

    frontend react project uploaded
    created mealmate frontend react project

---
+++ mealmateFrontend/src/Signup.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Signup() {
+    return <div className="Signup d-flex justify-content-center align-items-center 100-w vh-100 bg-white"
+    >
+        hello, sign up page
+    </div>
+
+}
+
+export default Signup;
