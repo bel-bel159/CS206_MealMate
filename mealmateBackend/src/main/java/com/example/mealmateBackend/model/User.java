ede75d587366c8b49d6295fb49fe5175481be0f5
kanekidawn <105071982+kanekidawn@users.noreply.github.com>
2024-03-05 19:51:10 +0800

    Update user entity

--- mealmateBackend/src/main/java/com/example/mealmateBackend/model/User.java
+++ mealmateBackend/src/main/java/com/example/mealmateBackend/model/User.java
@@ -7,12 +7,9 @@ import javax.persistence.Id;
 
 @Entity
 public class User {
-
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
     private String email;
+    private String name;
     private String password;
     private int age;
 
