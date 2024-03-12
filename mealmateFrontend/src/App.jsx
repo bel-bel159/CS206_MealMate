02573313e8fdf5ca29a5786a21480cf70ec2298f
Eaint <haeaint.2022@scis.smu.edu.sg>
2024-03-12 14:05:42 +0800

    fixed: navbar don't show on login, signup and notifications

--- mealmateFrontend/src/App.jsx
+++ mealmateFrontend/src/App.jsx
@@ -11,12 +11,15 @@ import Cart from "./Cart.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import ShowNavbar from "./ShowNavbar.jsx";
 
 function App() {
   return (
-    
     <BrowserRouter>
-      <Navbar />
+      <ShowNavbar>
+        <Navbar />
+      </ShowNavbar>
+
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/signup" element={<Signup />}></Route>
---
+++ mealmateFrontend/src/ShowNavbar.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState} from "react";
+import {useLocation} from "react-router-dom";
+
+const ShowNavbar = ({children}) => {
+    const location = useLocation();
+
+    const[showNavBar, setShowNavBar] = useState(false);
+    useEffect(() => {
+        console.log('this is location:', location)
+        if(location.pathname === '/login' || location.pathname === '/signup' || location.pathname === '/notification'){
+            setShowNavBar(false);
+        }else{
+            setShowNavBar(true);
+        }
+    },[location]);
+
+    
+    return( 
+        <div>{showNavBar && children}</div>
+    )
+}
+export default ShowNavbar;
