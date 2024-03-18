f236f286ef69ecd214e3a2c8dfbea58b6cee1067
vlingvling <vera.ling.2022@scis.smu.edu.sg>
2024-03-19 01:40:44 +0800

    navbar edits for orderer and new navbar for deliverers

--- mealmateFrontend/src/components/DeliverBottomNavbar.jsx
+++ mealmateFrontend/src/components/DeliverBottomNavbar.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { BottomNavigation, BottomNavigationAction } from "@mui/material";
 import home from "../assets/home.svg";
-import heart from "../assets/heart.svg";
+import calendar from "../assets/calendar.png";
 import user from "../assets/user.svg";
-import receipt from "../assets/receipt.svg";
+import moneyicon from "../assets/moneyicon.png";
 import { Link } from "react-router-dom";
 
-export const BottomNavbar = () => {
+export const DeliverBottomNavbar = () => {
   const [value, setValue] = React.useState(0);
 
   const handleNavigationChange = (event, newValue) => {
@@ -24,35 +24,36 @@ export const BottomNavbar = () => {
         padding: "10px",
       }}
       value={value}
-      onChange={handleNavigationChange}
+      onChange={(event, newValue) => handleNavigationChange(event, newValue)}
     >
+      {/* Correctly integrate Link with BottomNavigationAction */}
       <BottomNavigationAction
         label="Home"
         value="home"
         icon={<img src={home} alt="home" style={{ width: "30px", height: "auto" }} />}
         component={Link}
-        to="/"
+        to="/delivererHome"
       />
       <BottomNavigationAction
         label="Orders"
-        value="orders"
-        icon={<img src={receipt} alt="receipt" style={{ width: "30px", height: "auto" }} />}
+        value="orderSummary"
+        icon={<img src={calendar} alt="calendar" style={{ width: "30px", height: "auto" }} />}
         component={Link}
-        to="/orders"
+        to="/orderSummary"
       />
       <BottomNavigationAction
-        label="Favorites"
-        value="favorites"
-        icon={<img src={heart} alt="heart" style={{ width: "30px", height: "auto" }} />}
+        label="Earnings"
+        value="earning"
+        icon={<img src={moneyicon} alt="moneyicon" style={{ width: "30px", height: "auto" }} />}
         component={Link}
-        to="/favorites"
+        to="/earning"
       />
       <BottomNavigationAction
         label="Profile"
         value="profile"
         icon={<img src={user} alt="user" style={{ width: "30px", height: "auto" }} />}
         component={Link}
-        to="/profile"
+        to="/delivererProfile"
       />
     </BottomNavigation>
   );
